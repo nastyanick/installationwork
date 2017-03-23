10fc219f38700b9a6cadb6a9b616a4dadbaf9546
nastacia <rexby@ecwid.com>
2017-03-23 18:42:36 +0400

    add separate modules

--- presentation/src/main/java/com/nastynick/installationworks/view/LoginActivity.java
+++ presentation/src/main/java/com/nastynick/installationworks/view/LoginActivity.java
@@ -1,14 +1,20 @@
-package com.nastynick.installationworks;
+package com.nastynick.installationworks.view;
 
+import android.content.SharedPreferences;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.nastynick.installationworks.R;
 import com.nastynick.installationworks.databinding.ActivityLoginBinding;
+
+import javax.inject.Inject;
 
 
 public class LoginActivity extends AppCompatActivity {
+    @Inject SharedPreferences sharedPreferences;
+
     ActivityLoginBinding binding;
 
     @Override
